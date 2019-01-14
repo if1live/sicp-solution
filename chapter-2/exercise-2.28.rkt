2f57a261d2459177bef040c72abeff28fee8d3a8
/usr/lib/libsora.so <libsora25@gmail.com>
2019-01-15 00:06:32 +0900

    Update exercise-2.28.rkt

--- chapter-2/exercise-2.28.rkt
+++ chapter-2/exercise-2.28.rkt
@@ -1,6 +1,3 @@
-#!/usr/bin/env racket
-#lang scheme
-
 (define (fringe tree)
   (define (iter items results)
     (cond ((null? items) results)
@@ -19,10 +16,18 @@
                         (iter (cdr items) (list))))))
   (iter tree (list)))        
 
+(define (fringe3 tree)
+  (cond ((null? tree) '())
+        ((pair? tree) (append (fringe3 (car tree)) (fringe3 (cdr tree))))
+        (else (list tree))))
+
 (define x (list (list 1 2) (list 3 4)))
 
-(fringe x)
-(fringe (list x x))
+(print (fringe x))
+(print (fringe (list x x)))
 
-(fringe2 x)
-(fringe2 (list x x))
+(print (fringe2 x))
+(print (fringe2 (list x x)))
+
+(print (fringe3 x))
+(print (fringe3 (list x x)))
